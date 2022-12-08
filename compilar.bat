a05f925e4f74a2890fedfb5279d10cacc27c819b
josevdr95new <119988266+josevdr95new@users.noreply.github.com>
2022-12-08 18:02:43 -0500

    PokemmoUtilBar
    PokemmoUtilBar

---
+++ compilar.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+CLS
+
+REM Run NeoBook to compile the publication
+"C:\Program Files (x86)\VisualNeoWin\VisualNeoWin.exe" /C "D:\De neoBook\pokemmo_utilBar\PokemmoUtilBar.pub"
+
+REM Run the compiled publication
+REM "C:\Users\jose\Documents\NeoPlugins\npNots\Samples\Notifications.exe"
+
+REM Run NeoBook with the publication opened
+REM "C:\Program Files (x86)\VisualNeoWin\VisualNeoWin.exe" "D:\De neoBook\pokemmo_utilBar\PokemmoUtilBar.pub"
